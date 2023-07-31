d9c5b2ec9a1fbd9cd88b9c2983231ae7fd11e801
Dan Yunin <DaniilUnin2002@yandex.ru>
2023-07-31 12:59:25 +0300

    Ready task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  const arrayDigits = String(n).split('').reverse();
+  if (arrayDigits[arrayDigits.length - 1] === '-') {
+    arrayDigits.pop();
+  }
+  return arrayDigits.join('');
 }
